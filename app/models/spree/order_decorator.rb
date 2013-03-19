0cd9554604fbcfecf8502805699e6d0a896aef70
Gregor MacDougall <gmacdougall@freerunningtechnologies.com>
2013-03-19 14:56:47 -0700

    Use initial_payment_amount correctly...
    Change-Id: Id9eb3e9f9f07779103787f774a7e3a00fe3ab13d

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Order.class_eval do
   def initial_payment_total
-    line_items.map(&:preorder_amount).compact.inject(:+)
+    line_items.map(&:initial_payment_amount).compact.inject(:+)
   end
 end
--- spec/models/spree/order_decorator_spec.rb
+++ spec/models/spree/order_decorator_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe 'Spree::Order' do
   let(:order) { build :order, line_items: line_items }
-  let(:line_items) { [mock_model(Spree::LineItem, preorder_amount: 1), mock_model(Spree::LineItem, preorder_amount: 2.50), mock_model(Spree::LineItem, preorder_amount: nil)] }
+  let(:line_items) { [mock_model(Spree::LineItem, initial_payment_amount: 1), mock_model(Spree::LineItem, initial_payment_amount: 2.50), mock_model(Spree::LineItem, initial_payment_amount: nil)] }
 
   describe '.initial_payment_total' do
-    it 'returns the total for the preorder_amount for all line_items' do
+    it 'returns the total for the initial_payment_amount for all line_items' do
       order.initial_payment_total.should == 3.50
     end
   end
